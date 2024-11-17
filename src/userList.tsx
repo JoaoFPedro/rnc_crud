1f54bb80df2e8c24795f09cbe9e2336a0b4bbc90
Joao Pedro <joao.fpsilva98@gmail.com>
2024-11-17 19:43:16 -0300

    reaect navigation

---
+++ src/userList.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function userList() {
+  return (
+    <View>
+      <Text>UserList</Text>
+    </View>
+  )
+}
