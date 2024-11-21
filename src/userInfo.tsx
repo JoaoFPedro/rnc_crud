c3de734294cb323e14d61fc869166f08ad0c60eb
Joao Pedro <joao.fpsilva98@gmail.com>
2024-11-21 20:22:23 -0300

    refac: changing files names

--- src/userInfo.tsx
+++ src/userInfo.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function userInfo() {
+export default function UserInfo() {
   return (
     <View>
       <Text>userInfo</Text>
--- src/userList.tsx
+++ src/userList.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function userList() {
+export default function UserList() {
   return (
     <View>
       <Text>UserList</Text>
