734dd20ea39dd8ddf4ebf7743a16921a6aa02289
Joao Pedro <joao.fpsilva98@gmail.com>
2024-11-21 20:22:14 -0300

    adding button and creating route to useForm

--- App.tsx
+++ App.tsx
@@ -2,20 +2,37 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import userList from "./src/userList";
-import userInfo from "./src/userInfo";
+import { Button, Icon } from "@rneui/base";
+import UserList from "./src/UserList";
+import UserInfo from "./src/UserInfo";
+
+
 
 export type RootStackParamList = {
-  userList: undefined; // A tela "Home" não espera parâmetros
-  userInfo: { itemId: number; otherParam?: string }; // A tela "Details" espera parâmetros
+  UserList: undefined; // A tela "Home" não espera parâmetros
+  UserInfo: { itemId: number; otherParam?: string }; // A tela "Details" espera parâmetros
 };
 export default function App() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="userList">
-        <Stack.Screen name="userList" component={userList} />
-        <Stack.Screen name="userInfo" component={userInfo} />
+      <Stack.Navigator initialRouteName="UserList" screenOptions={{
+        headerStyle:{
+          backgroundColor: '#3072a8',
+        },
+        headerTintColor: '#fff'
+      }}>
+        <Stack.Screen name="UserList" component={UserList} options={({navigation}) => {
+          return {
+            title: 'Lista de usuários',
+            headerRight: () => (
+              <Button title='+' size="md" color='none' onPress={()=> navigation.navigate('UserInfo',  { itemId: 42, otherParam: 'Detalhes' })} >
+                <Icon name="add" size={25}color='white'/>
+              </Button>
+            )
+          }
+        }}  />
+        <Stack.Screen name="UserInfo" component={UserInfo} />
       </Stack.Navigator>
     </NavigationContainer>
   );
