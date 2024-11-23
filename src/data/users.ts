590c7d99022ad11cb58480d66c2384b4171904ff
Joao Pedro <joao.fpsilva98@gmail.com>
2024-11-23 12:26:50 -0300

    users mock

---
+++ src/data/users.ts
@@ -0,0 +1,42 @@
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
+  
+
+const users: User[] = [
+    {
+        id: 1,
+        name: 'Joao Pedro',
+        email: 'jones.pones@gmail.com'
+
+    },
+    {
+        id: 2,
+        name:'Gabriel George',
+        email: 'jones.pones@gmail.com'
+
+    },
+    {
+        id: 3,
+        name:'Norvina Ferreira',
+        email: 'jones.pones@gmail.com'
+
+    },
+    {
+        id: 4,
+        name: 'Claudia Rogeria',
+        email: 'jones.pones@gmail.com'
+
+    },
+    {
+        id: 5,
+        name: 'Fabio Magalhaes',
+        email: 'jones.pones@gmail.com'
+
+    }
+]
+
+export default users
