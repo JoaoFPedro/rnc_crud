96b0d28f6f17cd0c4064b1eed493670d6b3b532d
Joao Pedro <joao.fpsilva98@gmail.com>
2024-11-23 12:27:05 -0300

    identation

--- App.tsx
+++ App.tsx
@@ -1,38 +1,60 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Button, Icon } from "@rneui/base";
 import UserList from "./src/UserList";
 import UserForm from "./src/UserForm";
-
-
+import users, { User } from "./src/data/users";
 
 export type RootStackParamList = {
-  UserList: undefined; // A tela "Home" não espera parâmetros
+  UserList:{ users:User[]}; // A tela "Home" não espera parâmetros
   UserForm: { itemId: number; otherParam?: string }; // A tela "Details" espera parâmetros
 };
 export default function App() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserList" screenOptions={{
-        headerStyle:{
-          backgroundColor: '#3072a8',
-        },
-        headerTintColor: '#fff'
-      }}>
-        <Stack.Screen name="UserList" component={UserList} options={({navigation}) => {
-          return {
-            title: 'Lista de usuários',
-            headerRight: () => (
-              <Button title='+' size="md" color='none' onPress={()=> navigation.navigate('UserForm',  { itemId: 42, otherParam: 'Detalhes' })} >
-                <Icon name="add" size={25}color='white'/>
-              </Button>
-            )
-          }
-        }}  />
-        <Stack.Screen name="UserForm" component={UserForm} />
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#3072a8",
+          },
+          headerTintColor: "#fff",
+        }}
+      >
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          initialParams={{ users }}
+          options={({ navigation }) => {
+            return {
+              title: "Lista de usuários",
+              headerRight: () => (
+                <Button
+                  title="+"
+                  size="md"
+                  color="none"
+                  onPress={() =>
+                    navigation.navigate("UserForm", {
+                      itemId: 42,
+                      otherParam: "Detalhes",
+                    })
+                  }
+                >
+                  <Icon name="add" size={25} color="white" />
+                </Button>
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: "Formulario de usuários",
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
