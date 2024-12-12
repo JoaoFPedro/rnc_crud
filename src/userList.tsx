b4fc7dfa45a894d6c8899f4bc12a17574823fede
Joao Pedro <joao.fpsilva98@gmail.com>
2024-12-12 19:52:03 -0300

    connection to the api

--- src/userList.tsx
+++ src/userList.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image } from "react-native";
+import React, { useEffect } from "react";
+
+import { api } from "./services/api";
 
 export default function UserList() {
+
+  useEffect(() => {
+   
+    loadCostumer()
+    console.log(loadCostumer())
+  }, [])
+
+  async function loadCostumer (){ 
+    console.log('CHAMOU')
+    const response = await api.get('/listCustomer')
+
+    console.log('customers**', response.data)
+  }
+
+
+
   return (
     <View>
-      <Text>UserList</Text>
+      
     </View>
-  )
+  );
 }
