e5c6103576b7328b93644e4b3363c90bc0b52391
Joao Pedro <joao.fpsilva98@gmail.com>
2024-12-12 19:52:47 -0300

    removing mock

--- src/data/users.ts
+++ src/data/users.ts
@@ -3,6 +3,7 @@ export interface User {
     id: number;
     name: string;
     email: string;
+    avatarUrl: string
   }
   
 
@@ -10,31 +11,40 @@ const users: User[] = [
     {
         id: 1,
         name: 'Joao Pedro',
-        email: 'jones.pones@gmail.com'
+        email: 'jones.pones@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_640.png'
 
     },
     {
         id: 2,
         name:'Gabriel George',
-        email: 'jones.pones@gmail.com'
+        email: 'jones.pones@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_640.png'
+
 
     },
     {
         id: 3,
         name:'Norvina Ferreira',
-        email: 'jones.pones@gmail.com'
+        email: 'jones.pones@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_640.png'
+
 
     },
     {
         id: 4,
         name: 'Claudia Rogeria',
-        email: 'jones.pones@gmail.com'
+        email: 'jones.pones@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_640.png'
+
 
     },
     {
         id: 5,
         name: 'Fabio Magalhaes',
-        email: 'jones.pones@gmail.com'
+        email: 'jones.pones@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_640.png'
+
 
     }
 ]
